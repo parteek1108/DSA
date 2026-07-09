4ffffb015a2113f50fb867f53dd27e7cbbe4ed24
parteek1108 <parteek1108@gmail.com>
2026-07-09 09:18:13 +0530

    Time: 1 ms (10.04%) | Memory: 48 MB (47.34%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,29 @@
+class Solution {
+
+    public double pow(double x, long n) {
+
+        if (n == 0) {
+            return 1;
+        }
+
+        double half = pow(x, n / 2);
+
+        if (n % 2 == 0) {
+            return half * half;
+        } else {
+            return x * half * half;
+        }
+    }
+
+    public double myPow(double x, int n) {
+
+        long N = n;
+
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        return pow(x, N);
+    }
+}

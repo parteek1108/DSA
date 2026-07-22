9b9049e44b105ceb9bfe03e07b5877bfd2ad7f7a
parteek1108 <parteek1108@gmail.com>
2026-07-22 15:43:16 +0530

    Time: 1 ms (14.69%) | Memory: 41.9 MB (92.21%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,35 +1,34 @@
-import java.util.Arrays;
-
 class Solution {
-
-    public int solve(int i, int j, int[][] dp) {
-
-        if (i == 0 && j == 0) {
-            return 1;
-        }
-
-        if (i < 0 || j < 0) {
-            return 0;
-        }
-
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-
-        int up = solve(i - 1, j, dp);
-        int left = solve(i, j - 1, dp);
-
-        return dp[i][j] = up + left;
-    }
 
     public int uniquePaths(int m, int n) {
 
         int[][] dp = new int[m][n];
 
+        dp[0][0] = 1;
+
         for (int i = 0; i < m; i++) {
-            Arrays.fill(dp[i], -1);
+
+            for (int j = 0; j < n; j++) {
+
+                if (i == 0 && j == 0) {
+                    continue;
+                }
+
+                int up = 0;
+                int left = 0;
+
+                if (i > 0) {
+                    up = dp[i - 1][j];
+                }
+
+                if (j > 0) {
+                    left = dp[i][j - 1];
+                }
+
+                dp[i][j] = up + left;
+            }
         }
 
-        return solve(m - 1, n - 1, dp);
+        return dp[m - 1][n - 1];
     }
 }

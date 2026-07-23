539776cafdc10090fc15311e85127be7744336c3
parteek1108 <parteek1108@gmail.com>
2026-07-23 18:43:00 +0530

    Time: 5 ms (25.65%) | Memory: 50.4 MB (21.85%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,28 +1,41 @@
 class Solution {
-    public int solve( int i , int j , int[][] arr,int[][] dp ){
-        if(i==0 && j==0){
-            return arr[0][0];
-        }
-        if(i<0 || j<0){
-            return Integer.MAX_VALUE;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        int up = solve(i-1,j,arr,dp);
-        int down = solve(i,j-1,arr,dp);
-        return  dp[i][j] = arr[i][j]+Math.min(up,down);
-    }
+    // public int solve( int i , int j , int[][] arr,int[][] dp ){
+    //     if(i==0 && j==0){
+    //         return arr[0][0];
+    //     }
+    //     if(i<0 || j<0){
+    //         return Integer.MAX_VALUE;
+    //     }
+    //     if(dp[i][j]!=-1){
+    //         return dp[i][j];
+    //     }
+    //     int up = solve(i-1,j,arr,dp);
+    //     int down = solve(i,j-1,arr,dp);
+    //     return  dp[i][j] = arr[i][j]+Math.min(up,down);
+    // }
     public int minPathSum(int[][] arr) {
-        int sum =0;
         int m = arr.length;
         int n = arr[0].length;
         int[][] dp = new int[m][n];
-        for(int i = 0;i<arr.length;i++){
-            Arrays.fill(dp[i],-1);
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0&&j==0){
+                    dp[i][j]=arr[i][j];
+                    continue;
+                }
+                int up = Integer.MAX_VALUE;
+                int down = Integer.MAX_VALUE;
+                if(i>0){
+                     up = dp[i-1][j];
+                }
+                if(j>0) down = dp[i][j-1];
+                dp[i][j]=arr[i][j]+Math.min(up,down);
+            }
         }
         
-        return solve(m-1,n-1,arr,dp);
+        
+        return dp[m-1][n-1];
         
     }
 }

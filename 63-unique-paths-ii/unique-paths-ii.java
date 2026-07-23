105da58ed800ade76f15d3c7e5059246f0a893c0
parteek1108 <parteek1108@gmail.com>
2026-07-23 17:08:34 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (45.66%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,31 +1,41 @@
 class Solution {
-    public int solve(int[][] arr , int m , int n,int[][] dp){
-        
-        if(m<0 || n<0){
-            return 0;
-        }
-        if(arr[m][n]==1){
-            return 0;
-        }
 
-        if(m==0&&n==0){
-            return 1;
-        }
-        if(dp[m][n]!=-1){
-            return dp[m][n];
-        }
-        int up = solve(arr,m-1,n,dp);
-        int down = solve(arr,m,n-1,dp);
-        return dp[m][n]=up+down;
-    }
     public int uniquePathsWithObstacles(int[][] arr) {
+
         int m = arr.length;
         int n = arr[0].length;
+
         int[][] dp = new int[m][n];
+
         for (int i = 0; i < m; i++) {
-            Arrays.fill(dp[i], -1);
+
+            for (int j = 0; j < n; j++) {
+
+                // Obstacle
+                if (arr[i][j] == 1) {
+                    dp[i][j] = 0;
+                    continue;
+                }
+
+                // Starting cell
+                if (i == 0 && j == 0) {
+                    dp[i][j] = 1;
+                    continue;
+                }
+
+                int up = 0;
+                int left = 0;
+
+                if (i > 0)
+                    up = dp[i - 1][j];
+
+                if (j > 0)
+                    left = dp[i][j - 1];
+
+                dp[i][j] = up + left;
+            }
         }
-        
-        return solve(arr,m-1,n-1,dp);
+
+        return dp[m - 1][n - 1];
     }
 }

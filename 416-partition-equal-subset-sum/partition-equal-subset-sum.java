6cd309d3dfc0eee0e16c40e36881b7905b15c676
parteek1108 <parteek1108@gmail.com>
2026-07-25 11:49:09 +0530

    Time: 48 ms (54.91%) | Memory: 47.6 MB (54.11%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,34 +1,32 @@
 class Solution {
-    public boolean solve(int[] arr, int target, int index, Boolean dp[][]) {
-        if (target == 0) {
-            return true;
-        }
-        if (target < 0) {
-            return false;
-        }
-        if (index == 0) {
-            if (arr[index] == target) {
-                return true;
-            }
-            else{
-                return false;
-            }
-        }
-        if(dp[index][target]!=null){
-            return dp[index][target];
-        }
-        boolean take = false;
-        if (target >= arr[index]) {
-            take = solve(arr, target - arr[index], index - 1,dp);
-        }
-        boolean donttake = solve(arr, target, index - 1,dp);
+    // public boolean solve(int[] arr, int target, int index, Boolean dp[][]) {
+    //     if (target == 0) {
+    //         return true;
+    //     }
+    //     if (target < 0) {
+    //         return false;
+    //     }
+    //     if (index == 0) {
+    //         if (arr[index] == target) {
+    //             return true;
+    //         }
+    //         else{
+    //             return false;
+    //         }
+    //     }
+    //     if(dp[index][target]!=null){
+    //         return dp[index][target];
+    //     }
+    //     boolean take = false;
+    //     if (target >= arr[index]) {
+    //         take = solve(arr, target - arr[index], index - 1,dp);
+    //     }
+    //     boolean donttake = solve(arr, target, index - 1,dp);
 
-        return dp[index][target] = take || donttake;
-    }
-
+    //     return dp[index][target] = take || donttake;
+    // }
     public boolean canPartition(int[] arr) {
         int n = arr.length;
-        
         int sum = 0;
         for (int i = 0; i < n; i++) {
             sum += arr[i];
@@ -37,7 +35,31 @@ class Solution {
             return false;
         }
         int target = sum/2;
-        Boolean dp[][] = new Boolean[n][target+1];
-        return solve(arr, target, n - 1,dp);
+        //Boolean dp[][] = new Boolean[n][target+1];
+        //return solve(arr, target, n - 1,dp);
+        boolean dp[][] = new boolean[n][target+1];
+        for(int i=0;i<n;i++){
+            dp[i][0] = true; 
+        }
+        if(arr[0]<=target){
+            dp[0][arr[0]]=true;
+        }
+
+        for (int index = 1; index < n; index++) {
+            for (int t = 1; t <= target; t++) {
+
+                boolean notTake = dp[index - 1][t];
+
+                boolean take = false;
+
+                if (arr[index] <= t) {
+                    take = dp[index - 1][t - arr[index]];
+                }
+
+                dp[index][t] = take || notTake;
+            }
+        }
+
+        return dp[n - 1][target];
     }
 }

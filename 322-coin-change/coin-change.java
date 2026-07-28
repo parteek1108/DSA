dcece23d16459330d2962db1c532ca2eefb89209
parteek1108 <parteek1108@gmail.com>
2026-07-28 12:49:00 +0530

    Time: 20 ms (36.35%) | Memory: 46.9 MB (34.61%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,31 +1,31 @@
 class Solution {
-    public int solve(int[] arr, int amount, int index,int[][] dp){
-        if(index==0){
-            if(amount%arr[index]==0){
-                return amount/arr[index];
+    public int solve(int[] arr, int amount, int index, int[][] dp) {
+        if (index == 0) {
+            if (amount % arr[index] == 0) {
+                return amount / arr[index];
             }
-            
-            else{
+
+            else {
                 return Integer.MAX_VALUE;
             }
         }
-        if(dp[index][amount]!=-1){
+        if (dp[index][amount] != -1) {
             return dp[index][amount];
         }
-        int take =Integer.MAX_VALUE;
-        if(arr[index]<=amount){
-             int res =solve(arr,amount-arr[index],index,dp);
-            if(res!=Integer.MAX_VALUE){
-                take = res+1;
+        int take = Integer.MAX_VALUE;
+        if (arr[index] <= amount) {
+            int res = solve(arr, amount - arr[index], index, dp);
+            if (res != Integer.MAX_VALUE) {
+                take = res + 1;
             }
         }
-        int nontake = 0+solve(arr,amount,index-1,dp);
-        return dp[index][amount] = Math.min(take,nontake);
+        int nontake = 0 + solve(arr, amount, index - 1, dp);
+        return dp[index][amount] = Math.min(take, nontake);
     }
 
     public int coinChange(int[] arr, int amount) {
         int n = arr.length;
-        int[][] dp = new int[n][amount+1];
+        //int[][] dp = new int[n][amount+1];
         // for(int i =0;i<n;i++){
         //     Arrays.fill(dp[i],-1);
         // }
@@ -34,32 +34,42 @@ class Solution {
         //     return -1;
         // }
         // return ans;
-        for(int i =0;i<=amount;i++){
-            if(i%arr[0]==0){
-                dp[0][i]= i / arr[0];
-            }
-            else{
-                dp[0][i] = Integer.MAX_VALUE;
+        int[] prev = new int[amount + 1];
+        int[] curr = new int[amount + 1];
+        for (int i = 0; i <= amount; i++) {
+            if (i % arr[0] == 0) {
+                prev[i] = i / arr[0];
+            } else {
+                prev[i] = Integer.MAX_VALUE;
             }
         }
-        for(int i=1;i<n;i++){
-            for(int a = 0;a<=amount;a++){
+        for (int i = 1; i < n; i++) {
+
+            curr = new int[amount + 1];
+
+            for (int a = 0; a <= amount; a++) {
+
                 int take = Integer.MAX_VALUE;
-                if(arr[i]<=a){
-                    int res = dp[i][a-arr[i]];
-                    if(res!=Integer.MAX_VALUE){
-                        take = res+1;
+
+                if (arr[i] <= a) {
+                    int res = curr[a - arr[i]];
+
+                    if (res != Integer.MAX_VALUE) {
+                        take = res + 1;
                     }
                 }
-                int nontake = dp[i-1][a];
-                dp[i][a] = Math.min(take,nontake);
 
+                int notTake = prev[a];
+
+                curr[a] = Math.min(take, notTake);
             }
+
+            prev = curr.clone();
         }
-        if(dp[n-1][amount]==Integer.MAX_VALUE){
+        if (prev[amount] == Integer.MAX_VALUE) {
             return -1;
         }
-        return dp[n-1][amount];
+        return prev[amount];
 
     }
 }

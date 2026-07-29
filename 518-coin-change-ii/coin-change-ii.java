5852df0ed6d5be0b34939861def069801c99cafc
parteek1108 <parteek1108@gmail.com>
2026-07-29 10:22:48 +0530

    Time: 16 ms (36.25%) | Memory: 57.2 MB (22.41%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int solve(int amount, int[] arr, int index,int[][] dp) {
+        if (index == 0) {
+            if (amount%arr[0] == 0) {
+                return 1;
+            }
+            else{
+                return 0;
+            }
+            
+        }
+        if(dp[index][amount]!=-1){
+            return dp[index][amount];
+        }
+        int take = 0;
+        if (arr[index] <= amount) {
+            take = solve(amount - arr[index], arr, index,dp);
+
+        }
+        int nottake = solve(amount, arr, index-1,dp);
+        return dp[index][amount]= take + nottake;
+    }
+
+    public int change(int amount, int[] arr) {
+        int n = arr.length;
+        int dp[][] = new int[n][amount+1];
+        for(int i =0;i<arr.length;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        return solve(amount, arr, n - 1,dp);
+    }
+}

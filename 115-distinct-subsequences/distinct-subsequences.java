3eb4984196d951c0f1f75bd8267f627f843b39fa
parteek1108 <parteek1108@gmail.com>
2026-08-04 12:20:12 +0530

    Time: 18 ms (76.32%) | Memory: 54.1 MB (74.07%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,23 +1,43 @@
 class Solution {
-    public int solve(String s, String t , int i , int j, int[][] dp ){
-        if(j<0) return 1;
-        if(i<0) return 0;
+    public int solve(String s, String t, int i, int j, int[][] dp) {
+        if (j == 0)
+            return 1;
+        if (i == 0)
+            return 0;
 
-        if(dp[i][j] != -1){
+        if (dp[i][j] != -1) {
             return dp[i][j];
         }
-        if(s.charAt(i)==t.charAt(j)){
-            return  dp[i][j]= solve(s,t,i-1,j-1,dp) + solve(s,t,i-1,j,dp);
+        if (s.charAt(i - 1) == t.charAt(j - 1)) {
+            return dp[i][j] = solve(s, t, i - 1, j - 1, dp) + solve(s, t, i - 1, j, dp);
         }
-          return dp[i][j] = solve(s,t,i-1,j,dp);
+        return dp[i][j] = solve(s, t, i - 1, j, dp);
     }
+
     public int numDistinct(String s, String t) {
         int n = s.length();
         int m = t.length();
-        int[][] dp = new int[n+1][m+1];
-        for(int i =0;i<n;i++){
-            Arrays.fill(dp[i],-1);
+        int[][] dp = new int[n + 1][m + 1];
+        // for(int i =0;i<n;i++){
+        //     Arrays.fill(dp[i],-1);
+        // }
+        // return solve(s,t,n,m,dp);
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
         }
-        return solve(s,t,n-1,m-1,dp);
+        for (int j = 1; j <= m; j++) {
+            dp[0][j] = 0;
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[n][m];
     }
 }

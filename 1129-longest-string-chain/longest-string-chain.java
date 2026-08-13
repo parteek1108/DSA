cca2000b92d8a2d9c686842512c803f2994fb03a
parteek1108 <parteek1108@gmail.com>
2026-08-13 11:22:19 +0530

    Time: 54 ms (39.21%) | Memory: 46.3 MB (75.49%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.java
+++ 1129-longest-string-chain/longest-string-chain.java
@@ -1,10 +1,9 @@
 class Solution {
 
     public boolean check(String s1, String s2) {
-        // s1 should be predecessor of s2
-        // length difference must be exactly 1
 
-        if (s2.length() - s1.length() != 1) {
+        // s1 should be predecessor of s2
+        if (s2.length() != s1.length() + 1) {
             return false;
         }
 
@@ -15,10 +14,9 @@ class Solution {
 
             if (s1.charAt(i) == s2.charAt(j)) {
                 i++;
-                j++;
-            } else {
-                j++;
             }
+
+            j++;
         }
 
         return i == s1.length();
@@ -28,7 +26,6 @@ class Solution {
 
         int n = words.length;
 
-        // Important: sort by length
         Arrays.sort(words, (a, b) -> a.length() - b.length());
 
         int[] dp = new int[n];
@@ -43,9 +40,7 @@ class Solution {
 
                 if (check(words[j], words[i])) {
 
-                    if (dp[j] + 1 > dp[i]) {
-                        dp[i] = dp[j] + 1;
-                    }
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
 

4414f37fcd5989fb7758fcc6d7ee4f00d29c9f92
parteek1108 <parteek1108@gmail.com>
2026-08-14 16:36:52 +0530

    Time: 22 ms (34.61%) | Memory: 45.3 MB (88.97%) - LeetSync

--- 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.java
+++ 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.java
@@ -1,71 +1,41 @@
 class Solution {
-
-    class Pair {
-        int len;
-        int count;
-
-        Pair(int len, int count) {
-            this.len = len;
-            this.count = count;
-        }
-    }
-
-    Pair[][] dp;
-
-    Pair solve(int[] nums, int index, int prev) {
-
-        // Base case
-        if (index == nums.length) {
-            return new Pair(0, 1);
-        }
-
-        // prev + 1 because prev can be -1
-        if (dp[index][prev + 1] != null) {
-            return dp[index][prev + 1];
-        }
-
-        // NOT TAKE
-        Pair notTake = solve(nums, index + 1, prev);
-
-        // TAKE
-        Pair take = new Pair(0, 0);
-
-        if (prev == -1 || nums[index] > nums[prev]) {
-            Pair next = solve(nums, index + 1, index);
-
-            take.len = 1 + next.len;
-            take.count = next.count;
-        }
-
-        Pair ans;
-
-        // Take gives longer subsequence
-        if (take.len > notTake.len) {
-            ans = take;
-        }
-
-        // Not take gives longer subsequence
-        else if (notTake.len > take.len) {
-            ans = notTake;
-        }
-
-        // Both have same maximum length
-        else {
-            ans = new Pair(
-                take.len,
-                take.count + notTake.count
-            );
-        }
-
-        return dp[index][prev + 1] = ans;
-    }
-
     public int findNumberOfLIS(int[] nums) {
 
         int n = nums.length;
 
-        dp = new Pair[n][n + 1];
+        int[] len = new int[n];
+        int[] count = new int[n];
+        for (int i = 0; i < n; i++) {
+            len[i] = 1;
+            count[i] = 1;
+        }
 
-        return solve(nums, 0, -1).count;
+        int maxLen = 1;
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < i; j++) {
+
+                if (nums[j] < nums[i]) {
+                    if (len[j] + 1 > len[i]) {
+                        len[i] = len[j] + 1;
+                        count[i] = count[j];
+                    }
+                    else if (len[j] + 1 == len[i]) {
+                        count[i] += count[j];
+                    }
+                }
+            }
+
+            maxLen = Math.max(maxLen, len[i]);
+        }
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (len[i] == maxLen) {
+                ans += count[i];
+            }
+        }
+
+        return ans;
     }
 }

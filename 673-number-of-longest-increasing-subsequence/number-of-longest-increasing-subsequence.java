b7d7bb6fd9751ea20bca6a6632b285e685cb2b82
parteek1108 <parteek1108@gmail.com>
2026-08-14 16:37:50 +0530

    Time: 18 ms (95.98%) | Memory: 45.2 MB (88.97%) - LeetSync

--- 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.java
+++ 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.java
@@ -5,33 +5,34 @@ class Solution {
 
         int[] len = new int[n];
         int[] count = new int[n];
+
+        int maxLen = 0;
+        int ans = 0;
+
         for (int i = 0; i < n; i++) {
+
             len[i] = 1;
             count[i] = 1;
-        }
-
-        int maxLen = 1;
-        for (int i = 0; i < n; i++) {
 
             for (int j = 0; j < i; j++) {
 
                 if (nums[j] < nums[i]) {
+
                     if (len[j] + 1 > len[i]) {
                         len[i] = len[j] + 1;
                         count[i] = count[j];
                     }
+
                     else if (len[j] + 1 == len[i]) {
                         count[i] += count[j];
                     }
                 }
             }
-
-            maxLen = Math.max(maxLen, len[i]);
-        }
-        int ans = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (len[i] == maxLen) {
+            if (len[i] > maxLen) {
+                maxLen = len[i];
+                ans = count[i];
+            }
+            else if (len[i] == maxLen) {
                 ans += count[i];
             }
         }
